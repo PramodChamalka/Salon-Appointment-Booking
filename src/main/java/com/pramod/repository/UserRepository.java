b43413c8b5dcb7d11193a3819d89c45d502d4616
PramodChamalka <shanuthpchamalka01@gmail.com>
2025-03-27 00:50:58 +0530

    UserRepository creation

---
+++ src/main/java/com/pramod/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.pramod.repository;
+
+/*Author : Pramod Chamalka*/
+
+import com.pramod.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+}
